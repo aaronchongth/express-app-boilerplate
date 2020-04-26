3fceab4f5712b886195548d7bdb96f8608336c42
Aaron Chong <aaronchongth@gmail.com>
2020-04-27 01:37:54 +0800

    added more debugging tools and good practices

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+const createError = require('http-errors');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const PORT = process.env.PORT || 3000;
+const indexRouter = require('./routes/index');
 
-const routes = require('./routes/index');
+const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', routes);
+app.use('/', indexRouter);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
